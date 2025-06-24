da668f946816044c271d20d16ba6aabd73b1eea3
OliverTao98 <olivertao98@163.com>
2025-06-25 00:04:15 +0800

    chore: trend接口输出环境变量日志，排查线上环境问题

--- api/trend.js
+++ api/trend.js
@@ -1,3 +1,6 @@
+console.log('KV_REST_API_URL:', process.env.KV_REST_API_URL);
+console.log('KV_REST_API_TOKEN:', process.env.KV_REST_API_TOKEN);
+
 const TREND_KEY = 'trend-doc-data';
 
 export default async function handler(req, res) {
