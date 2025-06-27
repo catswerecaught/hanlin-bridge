5f40788b005c99c150e04453f0cc6e0f4b6db937
OliverTao98 <olivertao98@163.com>
2025-06-27 21:52:24 +0800

    网站内容更新

--- users.js
+++ users.js
@@ -4,5 +4,10 @@ const users = [
   { name: '生物杨老师', username: 'user00002', password: '123456', vip: 'Pro会员', avatar: 'images/user00002.jpg', supreme: false, expire: '终身会员' },
   { name: '化学孙老师', username: 'user00003', password: '123456', vip: 'Pro会员', avatar: 'images/user00003.jpg', supreme: false, expire: '2025-12-31' },
   { name: '化学张老师', username: 'user00004', password: '123456', vip: 'Pro会员', avatar: 'images/user00004.jpg', supreme: false, expire: '2025-12-31' },
-  { name: '邬学长', username: 'goxuezhang', password: 'letmein', vip: '普通会员', avatar: 'images/user00005.jpg', supreme: false, expire: '2025-12-31' }
+  { name: '邬学长', username: 'user00005', password: '123456', vip: '普通会员', avatar: 'images/user00005.jpg', supreme: false, expire: '2025-12-31' },
+  { name: '李老师', username: 'user00006', password: '654321', vip: 'Pro会员', avatar: 'images/user00006.jpg', supreme: false, expire: '2026-06-30' },
+  { name: '王学姐', username: 'user00007', password: 'abcdef', vip: '普通会员', avatar: 'images/user00007.jpg', supreme: false, expire: '2025-09-15' },
+  { name: '赵同学', username: 'user00008', password: '888888', vip: 'Pro会员', avatar: 'images/user00008.jpg', supreme: false, expire: '2027-01-01' },
+  { name: '钱老师', username: 'user00009', password: 'qwerty', vip: 'Pro会员', avatar: 'images/user00009.jpg', supreme: true, expire: '终身会员' },
+  { name: '孙学长', username: 'user00010', password: 'sun123', vip: '普通会员', avatar: 'images/user00010.jpg', supreme: false, expire: '2025-12-31' }
 ]; 
