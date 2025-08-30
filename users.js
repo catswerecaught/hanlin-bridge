36cdb3240f1ac8effd5cac13e0074bec20626f3b
OliverTao98 <olivertao98@163.com>
2025-08-30 17:48:34 +0800

    chore(users): rename primary user display name to 'Oliver Tao' in users.js

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 // 全局用户数据，供 script.js 和 profile.js 共用
 const users = [
-  { name: '陶先生', username: 'taosir', password: '962777', vip: 'Pro会员', avatar: 'images/user00001.jpg', supreme: true, expire: '终身会员' },
+  { name: 'Oliver Tao', username: 'taosir', password: '962777', vip: 'Pro会员', avatar: 'images/user00001.jpg', supreme: true, expire: '终身会员' },
   { name: '生物杨老师', username: 'user00002', password: '123456', vip: 'Pro会员', avatar: 'images/user00002.jpg', supreme: false, expire: '终身会员' },
   { name: '化学孙老师', username: 'user00003', password: '123456', vip: 'Pro会员', avatar: 'images/user00003.jpg', supreme: false, expire: '2025-12-31' },
   { name: '化学张老师', username: 'user00004', password: '123456', vip: 'Pro会员', avatar: 'images/user00004.jpg', supreme: false, expire: '2025-12-31' },
