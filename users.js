1c65c0cb53e6ebafc177dc160a0716feae11b335
OliverTao98 <olivertao98@163.com>
2025-09-01 23:33:41 +0800

    feat(users): update user00009 profile and avatar image

--- users.js
+++ users.js
@@ -8,6 +8,6 @@ const users = [
   { name: 'BenLi', username: 'user00006', password: '123456', vip: '普通会员', avatar: 'images/user00006.jpg', supreme: false, expire: '2025-12-30' },
   { name: 'Tuebo Social', username: 'user00007', password: 'abcdef', vip: 'Pro会员', avatar: 'images/user00007.jpg', supreme: false, expire: '2025-09-15' },
   { name: 'Miraitowa', username: 'user00008', password: '888888', vip: '普通会员', avatar: 'images/user00008.jpg', supreme: false, expire: '2027-01-01' },
-  { name: 'iamJen', username: 'user00009', password: 'qwerty', vip: 'Pro会员', avatar: 'images/user00009.jpg', supreme: false, expire: '终身会员' },
+  { name: '鱼游太玄', username: 'user00009', password: 'chaotianda', vip: '普通会员', avatar: 'images/user00009.jpg', supreme: false, expire: '2027-01-01' },
   { name: '翰林桥官方', username: 'user00010', password: 'sun123', vip: 'Pro会员', avatar: 'images/user00010.jpg', supreme: false, expire: '2025-12-31' }
 ]; 
