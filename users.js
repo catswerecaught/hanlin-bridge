e58b84bde91d972c7903129d922c018d4377fd69
OliverTao98 <olivertao98@163.com>
2025-09-04 00:13:04 +0800

    data: 更新化学孙老师会员有效期为终身会员

--- users.js
+++ users.js
@@ -2,7 +2,7 @@
 const users = [
   { name: 'Oliver Tao', username: 'taosir', password: '962777', vip: 'Pro会员', avatar: 'images/user00001.jpg', supreme: true, expire: '终身会员' },
   { name: '生物杨老师', username: 'user00002', password: '123456', vip: 'Pro会员', avatar: 'images/user00002.jpg', supreme: false, expire: '终身会员' },
-  { name: '化学孙老师', username: 'user00003', password: '123456', vip: 'Pro会员', avatar: 'images/user00003.jpg', supreme: false, expire: '2025-12-31' },
+  { name: '化学孙老师', username: 'user00003', password: '123456', vip: 'Pro会员', avatar: 'images/user00003.jpg', supreme: false, expire: '终身会员' },
   { name: '化学张老师', username: 'user00004', password: '123456', vip: 'Pro会员', avatar: 'images/user00004.jpg', supreme: false, expire: '2025-12-31' },
   { name: '邬学长', username: 'user00005', password: '123456', vip: '普通会员', avatar: 'images/user00005.jpg', supreme: false, expire: '2025-12-31' },
   { name: 'BenLi', username: 'user00006', password: '123456', vip: '普通会员', avatar: 'images/user00006.jpg', supreme: false, expire: '2025-12-30' },
