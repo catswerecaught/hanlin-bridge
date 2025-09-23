b6389ed7742cd008bad1e857b3e26ca8e1cad829
OliverTao98 <olivertao98@163.com>
2025-09-23 20:57:32 +0800

    Append 10 users with sequential avatars images/user00011.png..user00020.png; keep original data intact

--- users.js
+++ users.js
@@ -9,5 +9,15 @@ const users = [
   { name: 'Tuebo Social', username: 'user00007', password: 'abcdef', vip: 'Pro会员', avatar: 'images/user00007.jpg', supreme: false, expire: '2025-09-15' },
   { name: 'Miraitowa', username: 'user00008', password: '888888', vip: '普通会员', avatar: 'images/user00008.jpg', supreme: false, expire: '2027-01-01' },
   { name: '鱼游太玄', username: 'user00009', password: 'chaotianda', vip: '普通会员', avatar: 'images/user00009.jpg', supreme: false, expire: '2027-01-01' },
-  { name: '翰林桥官方', username: 'user00010', password: 'sun123', vip: 'Pro会员', avatar: 'images/user00010.jpg', supreme: false, expire: '2025-12-31' }
+  { name: '翰林桥官方', username: 'user00010', password: 'sun123', vip: 'Pro会员', avatar: 'images/user00010.jpg', supreme: false, expire: '2025-12-31' },
+  { name: '用户11', username: 'user00011', password: '123456', vip: '普通会员', avatar: 'images/user00011.png', supreme: false, expire: '2026-12-31' },
+  { name: '用户12', username: 'user00012', password: '123456', vip: 'Pro会员', avatar: 'images/user00012.png', supreme: false, expire: '终身会员' },
+  { name: '用户13', username: 'user00013', password: 'abcdef', vip: '普通会员', avatar: 'images/user00013.png', supreme: false, expire: '2026-06-30' },
+  { name: '用户14', username: 'user00014', password: 'abcdef', vip: 'Pro会员', avatar: 'images/user00014.png', supreme: false, expire: '2027-01-01' },
+  { name: '用户15', username: 'user00015', password: '888888', vip: '普通会员', avatar: 'images/user00015.png', supreme: false, expire: '2026-09-30' },
+  { name: '用户16', username: 'user00016', password: '888888', vip: 'Pro会员', avatar: 'images/user00016.png', supreme: false, expire: '终身会员' },
+  { name: '用户17', username: 'user00017', password: 'chaotianda', vip: '普通会员', avatar: 'images/user00017.png', supreme: false, expire: '2026-12-31' },
+  { name: '用户18', username: 'user00018', password: 'sun123', vip: 'Pro会员', avatar: 'images/user00018.png', supreme: false, expire: '2027-03-31' },
+  { name: '用户19', username: 'user00019', password: 'abcdef', vip: '普通会员', avatar: 'images/user00019.png', supreme: false, expire: '2026-03-31' },
+  { name: '用户20', username: 'user00020', password: '123456', vip: 'Pro会员', avatar: 'images/user00020.png', supreme: false, expire: '终身会员' }
 ]; 
